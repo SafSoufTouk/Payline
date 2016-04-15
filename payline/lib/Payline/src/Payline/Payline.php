12e210a8b570a6ea4365ebbb059005ed69f6b218
safwen toukabri <safwen.toukabri@proxym-it.com>
2016-04-15 12:19:25 +0100

    first commit

---
+++ payline/lib/Payline/src/Payline/Payline.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once __DIR__ . '/../lib/paylineSDK.php';
+
+class Payline_Payline extends paylineSDK {
+
+    public function __construct($merchant_id, $access_key, $proxy_host, $proxy_port, $proxy_login, $proxy_password, $environment, $logPath) {
+        parent::__construct($merchant_id, $access_key, $proxy_host, $proxy_port, $proxy_login, $proxy_password, $environment, $logPath);
+    }
+
+}
+
+?>
